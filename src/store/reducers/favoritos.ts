5d612305bf6ae0aa69f7176f1a83e0b4953abc6d
Ramon_SFR <ramonsaviofontesrocha@gmail.com>
2024-09-06 18:20:09 -0300

    reducer dos favoritos

---
+++ src/store/reducers/favoritos.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+type Produto = {
+  id: number
+  nome: string
+}
+
+type FavoritosState = {
+  itens: Produto[]
+}
+
+const initialState: FavoritosState = {
+  itens: []
+}
+
+const favoritosSlice = createSlice({
+  name: 'favoritos',
+  initialState,
+  reducers: {
+    favoritar: (state, action: PayloadAction<Produto>) => {
+      const produto = action.payload
+      const produtoExiste = state.itens.find((p) => p.id === produto.id)
+
+      if (produtoExiste) {
+        state.itens = state.itens.filter((p) => p.id !== produto.id)
+      } else {
+        state.itens.push(produto)
+      }
+    }
+  }
+})
+
+export const { favoritar } = favoritosSlice.actions
+export default favoritosSlice.reducer
